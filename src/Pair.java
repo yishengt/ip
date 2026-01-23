f9a3b86ac58baf40d64cc72f13d9d7457d7c9d45
yisheng <yishengtoh@outlook.com>
2026-01-23 17:34:42 +0800

    Level-3: Add mark/unmark commands using mutable Pair class

--- src/Pair.java
+++ src/Pair.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+public class Pair<K, V> {
+    private K first;
+    private V second;
 
-public class Pair {
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public void setFirst(K first) {
+        this.first = first;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Pair)) return false;
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        return first.equals(other.first) && second.equals(other.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() * 31 + second.hashCode();
+    }
 }
--- src/Tyrone.java
+++ src/Tyrone.java
@@ -3,6 +3,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Tyrone {
     public static void main(String[] args) throws Exception {
         String art =
@@ -39,13 +42,42 @@ public class Tyrone {
         System.out.println(banner);
         String input = "";
         Scanner sc = new Scanner(System.in);
+        ArrayList<Pair<Integer, String>> arr = new ArrayList<>();
 
+        Integer arrIndex = 0;
 
         while (!input.equalsIgnoreCase("exit")) {
             System.out.print("> ");          // Command prompt
             input = sc.nextLine();     // Read user input
 
+            String markPattern = "^mark (\\d+)$";
+            String unmarkPattern = "^unmark (\\d+)$";
+
+            Integer command = processCommand(input);
+            if(command < 2){
+                char[] charArr = input.toCharArray();
+                Character index = charArr[charArr.length - 1];
+                arrIndex = Character.getNumericValue(index);
+                if(command == 0){
+                    arr.get(arrIndex - 1).setFirst(0);
+                    input = "mark";
+                } else if (command == 1) {
+                    arr.get(arrIndex - 1).setFirst(1);
+                    input = "unmark";
+                }
+
+            }
+
             switch (input.toLowerCase()) {
+
+                case "mark":
+                    System.out.println("Nice! I've marked this task as done");
+                    System.out.println("[X] " + arr.get(arrIndex -1).getSecond());
+                    break;
+                case "unmark":
+                    System.out.println("K, I've marked this task as not done yet:\n");
+                    System.out.println("[ ] " + arr.get(arrIndex -1).getSecond());
+                    break;
                 case "list":
                     System.out.println("list");
                     break;
@@ -55,14 +87,48 @@ public class Tyrone {
                 case "blah":
                     System.out.println("blah");
                     break;
+                case "return list":
+                    for(int i = 1; i<arr.size()+1; i++){
+                        if(arr.get(i-1).getFirst() == 1){
+                            System.out.print(i);
+                            System.out.print(". ");
+                            System.out.print("[ ] ");
+                            System.out.println(arr.get(i-1).getSecond());
+                        } else {
+                            System.out.print(i);
+                            System.out.print(". ");
+                            System.out.print("[X] ");
+                            System.out.println(arr.get(i-1).getSecond());
+                        }
+
+                    }
+                    break;
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
                 default:
-                    System.out.println("I don't understand that command");
+                    Pair<Integer, String> temp = new Pair<Integer, String>(1, input);
+                    arr.add(temp);
+                    System.out.println("added " + input);
             }
         }
 
         sc.close();
+    }
+
+    public static Integer processCommand(String input) {
+        Pattern markPattern = Pattern.compile("^mark (\\d+)$");
+        Pattern unmarkPattern = Pattern.compile("^unmark (\\d+)$");
+
+        Matcher markMatcher = markPattern.matcher(input);
+        Matcher unmarkMatcher = unmarkPattern.matcher(input);
+
+        if (markMatcher.matches()) {
+           return 0;
+        } else if (unmarkMatcher.matches()) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
